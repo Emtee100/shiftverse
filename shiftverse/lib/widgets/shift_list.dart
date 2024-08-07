3c9ef8c583089d632c4d3ec3f919fecdf7c44aa9
Emtee100 <mt9510037@gmail.com>
2024-08-07 11:51:49 +0300

    Bug fix: shifts tab was not displaying shift data in release mode

--- shiftverse/lib/widgets/shift_list.dart
+++ shiftverse/lib/widgets/shift_list.dart
@@ -18,49 +18,50 @@ class _ShiftListState extends State<ShiftList> {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return const SliverToBoxAdapter(
               child: Center(child: CircularProgressIndicator()));
-        } else if (snapshot.data!.isEmpty) {
-          return const Text('Select a filter to view shifts');
+        } else if (snapshot.data!.isEmpty || !snapshot.hasData) {
+          return const SliverToBoxAdapter(child: Text('Select a filter to view shifts'));
         } else if (snapshot.hasData) {
-          return SliverList.builder(
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) {
-                return Padding(
-                    padding: const EdgeInsets.only(bottom: 20),
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 25),
-                      child: ListTile(
-                          leading: const Icon(Icons.access_time),
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10)),
-                          tileColor: Theme.of(context)
-                              .colorScheme
-                              .surfaceContainerHigh,
-                          title: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
+          return SliverList(
+              delegate: SliverChildBuilderDelegate(
+                  childCount: snapshot.data!.length, (context, index) {
+            return Padding(
+                padding: const EdgeInsets.only(bottom: 20),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: ListTile(
+                      leading: const Icon(Icons.access_time),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      tileColor:
+                          Theme.of(context).colorScheme.surfaceContainerHigh,
+                      title: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text('Leaders on duty:'),
+                          Row(
                             children: [
-                              const Text('Leaders on duty:'),
-                              Row(
-                                children: [
-                                  Text(
-                                    '1. ${snapshot.data![index][2]}',
-                                  ),
-                                  const SizedBox(width: 10),
-                                  Text(
-                                    '2. ${snapshot.data![index][3]}',
-                                  ),
-                                ],
-                              ),
                               Text(
-                                'Date: ${snapshot.data![index][0]}',
+                                '1. ${snapshot.data![index][2]}',
                               ),
-                              Text('Jumuiya: ${snapshot.data![index][4]}'),
+                              const SizedBox(width: 10),
+                              Text(
+                                '2. ${snapshot.data![index][3]}',
+                              ),
                             ],
-                          )),
-                    ));
-              });
+                          ),
+                          Text(
+                            'Date: ${snapshot.data![index][0]}',
+                          ),
+                          Text('Jumuiya: ${snapshot.data![index][4]}'),
+                        ],
+                      )),
+                ));
+          }));
         } else if (snapshot.hasError) {
-          return const Center(
-              child: Text('An error occurred while retrieving data'));
+          return const SliverToBoxAdapter(
+            child: Center(
+                child: Text('An error occurred while retrieving data')),
+          );
         } else {
           return const Text('Null');
         }
